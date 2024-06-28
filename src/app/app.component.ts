28d79585188510a88450e3214dbd04969cdbcc4b
César González <cedgo1997@gmail.com>
2024-06-28 15:18:40 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventService } from './services/event.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'event-front';
+  userIds: string = '';
+  url: string = '';
+  events: any[] = [];
+
+  constructor(private eventService: EventService) {
+    this.eventService.getEvents().subscribe((event: any) => {
+      this.events.push(event);
+    });
+  }
+
+  connect(): void {
+    const userIdArray = this.userIds.split(',').map(id => parseInt(id.trim()));
+    const campaignIds = [1988];
+    this.eventService.closeEventSources();
+    userIdArray.forEach(userId => {
+      if (!isNaN(userId)) {
+        this.eventService.listenToCampaignsEvents(this.url, userId, campaignIds);
+      }
+    });
+  }
 }
---
+++ src/app/services/event.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+  constructor(private http: HttpClient) { }
+  private eventSources: { [key: string]: EventSource } = {};
+  private eventDataBS: Subject<any> = new Subject();
+  private eventErrorBS: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  listenToCampaignsEvents(url: string, userId: number, campaignIds: number[]): void {
+    const eventSourceKey = `${url}/events/generic/${userId}`;
+    this.eventSources[eventSourceKey] = new EventSource(`${eventSourceKey}?campaignIds=${campaignIds}`);
+
+    this.eventSources[eventSourceKey].addEventListener("Notification", (event: Partial<MessageEvent>) => {
+      this.eventDataBS.next(JSON.parse(event.data));
+    });
+  }
+
+  closeEventSources() {
+    for (const key in this.eventSources) {
+      if (this.eventSources[key]) {
+        this.eventSources[key].close();
+      }
+    }
+    this.eventSources = {};
+    this.eventDataBS.complete();
+    this.eventDataBS = new Subject();
+  }
+
+  getEvents(): Observable<any> {
+    return this.eventDataBS.asObservable();
+  }
+
+  getEventsError(): Observable<boolean> {
+    return this.eventErrorBS.asObservable();
+  }
+
+  setEventsError(status: boolean): void {
+    this.eventErrorBS.next(status);
+  }
+}
